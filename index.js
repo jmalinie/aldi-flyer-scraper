73420ccf5169c363fa7ac1dd8b9dc60e9520af13
jmalinie <38893005+jmalinie@users.noreply.github.com>
2025-04-30 16:03:11 +0300

    Fix Sanity client import

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+const { createClient } = require('@sanity/client');
+
 const sanity = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
   token: process.env.SANITY_API_TOKEN,
-  apiVersion: '2024-04-30',
+  apiVersion: '2024-04-30', // mutlaka API versiyonu belirt
   useCdn: false,
 });
 
